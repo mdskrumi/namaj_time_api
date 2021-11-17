ba572dca450922faacba84f6bd00b86f2ac716bd
mdskrumi-bs23 <sakibul.alam@brainstation-23.com>
2021-11-17 15:44:02 +0600

    added mongoose

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 // Config Files
 const config = require("./config");
@@ -13,6 +14,15 @@ app.use(bodyParser.json());
 
 app.use("/namaj", namajRoutes);
 
-app.listen(config.port, () =>
-  console.log(`Application Started. Listening at port ${config.port}`)
-);
+mongoose
+  .connect(
+    "mongodb+srv://root:gottaqdiewithiman_315@namajtimeapi.ri4nn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(config.port, () =>
+      console.log(`Application Started. Listening at port ${config.port}`)
+    );
+  })
+  .catch((err) => {
+    console.log("Database Error: ", err);
+  });
