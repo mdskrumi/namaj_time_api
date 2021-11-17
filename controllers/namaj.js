81d0c9ed7f8a8410db1413d803a481668bb35e2b
mdskrumi-bs23 <sakibul.alam@brainstation-23.com>
2021-11-17 18:21:09 +0600

    time managed for namaj

--- controllers/namaj.js
+++ controllers/namaj.js
@@ -1,22 +1,38 @@
 const request = require("request");
 
+// Helper Functions
+const { makeEasyModel } = require("../utils/makeEasyModel");
+
 const getDayTimeByLatLon = (req, res, next) => {
   const lat = 23.777176;
   const lon = 90.399452;
-
+  // &iso8601=true
   request(
     `http://api.aladhan.com/v1/calendar?latitude=${lat}&longitude=${lon}`,
     { json: true },
     (err, _, body) => {
       if (err) {
-        return res.json({
-          messgage: "ALLAHU AKBAR",
+        return res.status(500).json({
+          messgage: "We are having some problem. Please try again later.",
           error: err,
+          status: "fail",
+          status_code: 500,
         });
       } else {
-        return res.json({
-          messgage: "ALLAHU AKBAR",
-          data: body.data,
+        if (body.code === 200) {
+          makeEasyModel(body.data);
+          return res.status(200).json({
+            messgage: "You have successfully fetched the data.",
+            data: body.data,
+            status: "success",
+            status_code: 200,
+          });
+        }
+        return res.status(body.code).json({
+          messgage: "We are having some problem. Please try again later.",
+          error: body.data,
+          status: body.status,
+          status_code: body.code,
         });
       }
     }
---
+++ utils/makeEasyModel.js
@@ -0,0 +1,65 @@
+const formettedTime = (time) => time.split(" ")[0];
+
+const makeForbiddenTimeDiff = (time, diff) => {
+  const [h, m] = formettedTime(time).split(":");
+  let hour = parseInt(h);
+  let min = parseInt(m);
+  min = min + diff;
+  if (min >= 60) {
+    min = min % 60;
+    hour += 1;
+  } else if (min < 0) {
+    min = 60 + min;
+    hour -= 1;
+  }
+  let res = "";
+  if (hour <= 9) {
+    res += `0${hour}`;
+  } else {
+    res += `${hour}`;
+  }
+  res += ":";
+  if (min <= 9) {
+    res += `0${min}`;
+  } else {
+    res += `${min}`;
+  }
+  return res;
+};
+
+const manageTimings = (timings) => {
+  return {
+    fajr: formettedTime(timings.Fajr),
+    sunrise: formettedTime(timings.Sunrise),
+    forbiddenAfterFarj: makeForbiddenTimeDiff(timings.Sunrise, 20),
+    forbiddenBeforeDhuhr: makeForbiddenTimeDiff(timings.Dhuhr, -20),
+    dhuhr: formettedTime(timings.Dhuhr),
+    asr: formettedTime(timings.Asr),
+    forbiddenBeforeMaghrib: makeForbiddenTimeDiff(timings.Sunset, -20),
+    sunset: formettedTime(timings.Sunset),
+    maghrib: formettedTime(timings.Maghrib),
+    isha: formettedTime(timings.Isha),
+  };
+};
+
+const makeNamajObject = (data) => {
+  let model = {};
+  if (data) {
+    const { timings, date, meta } = data;
+    if (timings) model = { ...model, ...manageTimings(timings) };
+    console.log(model);
+  }
+};
+
+const makeEasyModel = (data) => {
+  const modifiedData = [];
+  if (data && data.length > 0) {
+    data.forEach((element) => {
+      makeNamajObject(element);
+    });
+  }
+};
+
+module.exports = {
+  makeEasyModel,
+};
